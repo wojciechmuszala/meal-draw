1c61705225701429fde9679b1083318cb253ce72
Wojciech Muszala <wojciech.muszala@gmail.com>
2023-05-09 18:55:53 +0200

    rebuild dishes array (add to dishList class), rebuild isDishonList function, add code comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,25 @@
+//data base based on indexedDB import form other file
 import * as dishListDB from "./dishListDB.js";
 let db;
 
+// declaration of constants
 const BREAKFAST_TYPE = "breakfast";
 const BRUCH_TYPE = "brunch";
 const LUNCH_TYPE = "lunch";
 const SUPPER_TYPE = "supper";
 const MATH_RANDOM_MULTIPLIER = 10000000000000000;
 
+//other declarations
 const getButton = document.getElementById("draw-button");
+// let dishes = [];
 
-let dishes = [];
+function isDishOnList(dish, list) {
+    return list.some((dishOnList) => {
+        return JSON.stringify(dishOnList) === JSON.stringify(dish);
+    });
+}
 
+//dish object class
 class Dish {
     constructor(name, type) {
         this.name = name;
@@ -18,6 +27,7 @@ class Dish {
     }
 }
 
+//handling of basic messages
 class StatusHandler {
     constructor(querySelector) {
         this.element = document.querySelector(querySelector);
@@ -39,39 +49,47 @@ class StatusHandler {
         this.element.innerHTML = "";
     }
 }
+
+// dish list class
 class DishList {
     constructor() {
+        this.dishes = [];
+        const self = this;
         this.addDishButton = document.getElementById("add-new-dish-button");
         this.showButton = document.querySelector(".dish-list__show");
         this.containerElement = document.querySelector(".dish-list");
     }
 
+    // show list container element
     showList() {
         this.containerElement.classList.toggle("dish-list--active");
     }
 
+    // add new dish to list
     addNewDish() {
         const dishNameInput = document.getElementById("enter-new-dish-name");
         const dishTypeSelect = document.getElementById("enter-new-dish-type");
         const statusHandler = new StatusHandler(".add__status-info");
 
+        // clear previous messages
         statusHandler.reset();
 
+        // get dish from input
         const dishName = dishNameInput.value.trim();
+        // --- check if input is empty
         if (dishName.trim() === "") {
             statusHandler.add("error", "Oops! Please complete all fields!");
             return;
         }
-
         const dishType = dishTypeSelect.value;
+        // --- cleaning input after data collection
         dishNameInput.value = "";
 
+        // creating a new dish based on the data collected from the input
         const newDish = new Dish(dishName, dishType);
 
-        const isDishOnList = dishes.some((dishOnList) => {
-            return JSON.stringify(dishOnList) === JSON.stringify(newDish);
-        });
-        if (isDishOnList) {
+        // check if the dish has already been added + error handling
+        if (isDishOnList(newDish, this.dishes)) {
             statusHandler.add(
                 "error",
                 "Oops! Same dish of the same type is already on the list!"
@@ -79,20 +97,25 @@ class DishList {
             return;
         }
 
-        dishes.push(newDish);
-        statusHandler.add("success", "The dish has been added to the list!");
+        // adding dish to dish array
+        this.dishes.push(newDish);
 
+        // creating and database transaction
         const objStore = dishListDB.db
             .transaction("dishes", "readwrite")
             .objectStore("dishes");
 
+        // adding dish to database
         objStore.add({
             id: Math.random() * MATH_RANDOM_MULTIPLIER,
             name: dishName,
             type: dishType,
         });
 
-        //add new dish to dishList
+        //succes handling
+        statusHandler.add("success", "The dish has been added to the list!");
+
+        //creating new dish element in dish list container
         const newDishElement = document.createElement("li");
         newDishElement.classList.add("dish-list__item");
         newDishElement.innerHTML = dishName;
@@ -101,8 +124,10 @@ class DishList {
     }
 }
 
-async function renderDishesFromDBToList() {
+// async function declaration because database operations are async
+async function renderDishesFromDBToList(list) {
     try {
+        // preparing the database for reading + creating indexes for searching the database
         db = await dishListDB.dbPromise;
         const objStore = db
             .transaction("dishes", "readwrite")
@@ -115,23 +140,22 @@ async function renderDishesFromDBToList() {
             typeIndex.getAll([SUPPER_TYPE]),
         ];
 
+        // loop through all type of dishes in database
         for (const typeQuery of typeQueryArray) {
             typeQuery.onsuccess = function () {
+                // loop through all dishes of currently searched type
                 typeQuery.result.forEach((dish) => {
                     const newDish = new Dish(dish.name, dish.type);
 
-                    const isDishOnList = dishes.some((dishOnList) => {
-                        return (
-                            JSON.stringify(dishOnList) ===
-                            JSON.stringify(newDish)
-                        );
-                    });
-                    if (isDishOnList) {
+                    // check if the dish has already been added
+                    if (isDishOnList(newDish, list.dishes)) {
                         return;
                     }
 
-                    dishes.push(newDish);
+                    // adding dish to dish array
+                    list.dishes.push(newDish);
 
+                    //creating new dish element in currently searched dish type list container
                     const newDishElement = document.createElement("li");
                     newDishElement.classList.add("dish-list__item");
                     newDishElement.innerHTML = newDish.name;
@@ -149,6 +173,7 @@ async function renderDishesFromDBToList() {
 
 const dishList = new DishList();
 
+// event listeners
 dishList.addDishButton.addEventListener(
     "click",
     dishList.addNewDish.bind(dishList)
@@ -170,4 +195,4 @@ dishList.showButton.addEventListener("click", dishList.showList.bind(dishList));
 
 // -------
 
-renderDishesFromDBToList();
+renderDishesFromDBToList(dishList);
